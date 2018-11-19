ed3fb3dca60863bc81f6ee89c769d599cde707ad
Genc Mazlami <genc.mazlami@zuehlke.com>
2018-11-19 10:18:47 +0100

    Initial cloud function commit

---
+++ functions/airport_counter.py
@@ -0,0 +1,11 @@
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/0.12/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/0.12/api/#flask.Flask.make_response>.
+    """
+    return 'Hello, CloudChallenge2018!'
