232627b8bdbd4e507e948cff8a504dac61bc5e2a
Genc Mazlami <genc.mazlami@zuehlke.com>
2018-11-19 12:31:53 +0100

    Initial bigquery integration

--- functions/main.py
+++ functions/main.py
@@ -1,11 +1,14 @@
-def airport_counter(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/0.12/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/0.12/api/#flask.Flask.make_response>.
-    """
-    return 'Hello, CloudChallenge2018!'
+from google.cloud import bigquery
+import logging
+
+DATASET_ID = 'cloud-hackathon-team-athena:flight_messages'
+
+def airport_counter(request):    
+    return query_bigquery('ZRH')
+
+def query_bigquery(airport_code):
+    # Instantiates a client
+    bigquery_client = bigquery.Client()
+    query_job = client.query("SELECT COUNT(*) FROM flight_messages.raw_flight_messages WHERE airport = " + airport_code)
+    results = query_job.result() 
+    return results[0]
